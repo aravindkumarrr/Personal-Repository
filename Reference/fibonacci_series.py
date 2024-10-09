5f1cec0269b4042ca41b17ff14dd908270d9c85e
aravindkumarrr <arkumar7055@gmail.com>
2024-10-09 15:41:08 +0530

    Create fibonacci_series.py

---
+++ Reference/fibonacci_series.py
@@ -0,0 +1,13 @@
+n=int(input("Enter the number of terms: "))
+a = 0
+b = 1
+next = b  
+count = 1
+
+while count <= n:
+    print(next, end=" ")
+    count += 1
+    a=b
+    b=next
+    next = a + b
+print()
