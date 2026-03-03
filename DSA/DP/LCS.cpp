d8b66b0e89d48dffc4acfbf45eb3f2b265f06190
Aravind Kumar <arkumar7055@gmail.com>
2026-03-03 14:54:12 +0530

    Simplified version

--- DSA/DP/LCS.cpp
+++ DSA/DP/LCS.cpp
@@ -1,50 +1,29 @@
 #include <iostream>
-#include <string>
 #include <vector>
-#include <algorithm>
-
 using namespace std;
 
 int main() {
-    string X, Y;
-    cout << "Enter string X: ";
-    cin >> X;
-    cout << "Enter string Y: ";
-    cin >> Y;
+    string s1, s2;
+    cin >> s1 >> s2;
 
-    int m = X.length();
-    int n = Y.length();
+    int n = s1.length();
+    int m = s2.length();
 
-    vector<vector<int>> LCS(m + 1, vector<int>(n + 1, 0));
+    // Create DP table of size (n+1) x (m+1)
+    vector<vector<int>> DP(n + 1, vector<int>(m + 1, 0));
 
-    for (int i = 1; i <= m; i++) {
-        for (int j = 1; j <= n; j++) {
-            if (X[i - 1] == Y[j - 1]) {
-                LCS[i][j] = 1 + LCS[i - 1][j - 1];
-            } else {
-                LCS[i][j] = max(LCS[i - 1][j], LCS[i][j - 1]);
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= m; j++) {
+            if(s1[i - 1] == s2[j - 1]) {
+                DP[i][j] = 1 + DP[i - 1][j - 1];
+            }
+            else {
+                DP[i][j] = max(DP[i - 1][j], DP[i][j - 1]);
             }
         }
     }
 
-    string lcs_str = "";
-    int i = m, j = n;
-    while (i > 0 && j > 0) {
-        if (X[i - 1] == Y[j - 1]) {
-            lcs_str += X[i - 1];
-            i--;
-            j--;
-        } else if (LCS[i - 1][j] >= LCS[i][j - 1]) {
-            i--;
-        } else {
-            j--;
-        }
-    }
-
-    reverse(lcs_str.begin(), lcs_str.end());
-
-    cout << "LCS Length: " << LCS[m][n] << endl;
-    cout << "LCS String: " << lcs_str << endl;
+    cout << DP[n][m];
 
     return 0;
 }
