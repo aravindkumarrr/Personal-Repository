11224bad84b9f120ee90d3bdaa03df50760bad11
Aravind Kumar <arkumar7055@gmail.com>
2026-03-05 20:26:42 +0530

    Update Kadane'sAlgorithm(Max Subarray).cpp

--- DSA/DAC/Kadane'sAlgorithm(Max Subarray).cpp
+++ DSA/DAC/Kadane'sAlgorithm(Max Subarray).cpp
@@ -1,7 +1,7 @@
 int maxSubArray(vector<int>& nums) {
     int sum = nums[0], maxSum = nums[0];
     for(int i = 1; i < nums.size(); i++) {
-        sum = max(sum + nums[i], nums[i]);
+        sum = max(sum + nums[i], nums[i]); // "Is it better to extend the current subarray or start new?"
         maxSum = max(maxSum, sum);
     }
     return maxSum;
