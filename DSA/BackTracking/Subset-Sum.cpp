968748d671e1f054a7fa54b0c4b76d7a8061316a
Aravind Kumar <arkumar7055@gmail.com>
2026-04-17 10:07:54 +0530

    Update Subset-Sum.cpp

--- DSA/BackTracking/Subset-Sum.cpp
+++ DSA/BackTracking/Subset-Sum.cpp
@@ -1,24 +1,25 @@
 #include <iostream>
 #include <vector>
-
-using namespace std;
-
-void findSubsets(vector<int>& nums, int target, int index, vector<int>& current, vector<vector<int>>& result) {
-    if (target == 0) {
+using namespace std; 
+// At a store with exactly $target, and want to spend every single bit - no more, no less.
+void findSubsets(vector<int>&nums, int target, int index, vector<int>&current, vector<vector<int>>& result){
+    if(target == 0){
+        // if you run out of EXACTLY how much you had -> success. Add this to solution.
         result.push_back(current);
         return;
     }
-    
-    if (target < 0 || index == nums.size()) {
+    if(target<0 || index==nums.size()){
+        // If you went over your budget OR ran out of items to buy -> failure.
         return;
     }
 
-    current.push_back(nums[index]);
-    findSubsets(nums, target - nums[index], index + 1, current, result);
-
+    current.push_back(nums[index]); // Put the current item onto your cart.
+    // For each item, you explore two parallel cases before moving on to next item: Include OR Exclude.
+    findSubsets(nums,target-nums[index],index+1,current,result); // Include and move on to next item.
+    // target - nums[index]: Means you spend on the current item (index), and move on to next item (index+1)
     current.pop_back();
-
-    findSubsets(nums, target, index + 1, current, result);
+    // We undo this and switch to the case where we exclude the item, by doing nothing with target and simply moving on to next item (index+1)
+    findSubsets(nums, target, index+1, current, result);
 }
 
 int main() {
