a5960cdb11975a78d4ddfb968506c41a4a448134
Aravind Kumar <arkumar7055@gmail.com>
2026-04-17 07:10:45 +0530

    Update N-Queens.cpp

--- DSA/BackTracking/N-Queens.cpp
+++ DSA/BackTracking/N-Queens.cpp
@@ -1,1 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
 
+using namespace std; 
+
+int n;
+vector<vector<int>> sols;
+
+bool safe(vector<int>& board, int row, int col) {
+    for(int i = 0; i < row; i++) {
+        if(board[i] == col || abs(board[i] - col) == abs(i - row)) {
+            return false;
+        }
+    }
+    return true;
+}
+
+void solve(vector<int>& board, int row) {
+    if(row == n) {
+        sols.push_back(board);
+        return;
+    }
+    for (int col = 0; col < n; col++) {
+        if(safe(board, row, col)) {
+            board[row] = col;
+            solve(board, row + 1);
+            board[row] = -1;
+        }
+    }
+}
+
+int main() {
+    n = 8;
+    vector<int> board(n, -1);
+    solve(board, 0);
+    cout << sols.size() << "\n";
+    return 0;
+}
